dc8589021c8ac2bda9a77beef80c50d5ae48fec0
pacificuwitonze406 <akalizaracia27@gmail.com>
2026-09-14 21:39:58 +0200

    corrected some errores for the migration to go through

--- lease/models.py
+++ lease/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from tenant.models import Tenant
+from property.models import Property
 
 class Lease(models.Model):
     PENDING = 'Pending Signature'
@@ -9,19 +11,19 @@ class Lease(models.Model):
     EXPIRED = 'Expired'
     TERMINATED = 'Terminated'
     Status_posted=[
-        (PENDING, 'Pending Signature')
-        (SIGNED, 'Signed / Future')
-        (ACTIVE, 'Active / Current')
-        (DELINQUENT, 'Past Due')
-        (NOTICE, 'Notice Given')
-        (EXPIRED, 'Expired')
-        (TERMINATED, 'Terminated')
+        (PENDING, 'Pending Signature'),
+        (SIGNED, 'Signed / Future'),
+        (ACTIVE, 'Active / Current'),
+        (DELINQUENT, 'Past Due'),
+        (NOTICE, 'Notice Given'),
+        (EXPIRED, 'Expired'),
+        (TERMINATED, 'Terminated'),
     ]
         
     lease_id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
-    property_id= models.ForeignKey('property_id.Property', on_delete=models.CASCADE)
-    tenant_id= models.ForeignKey('tenant_id.Tenant', on_delete=models.CASCADE)
+    property_id= models.ForeignKey(Property, on_delete=models.CASCADE)
+    tenant_id= models.ForeignKey(Tenant, on_delete=models.CASCADE)
     start_date = models.DateTimeField(auto_now_add=True)
     end_date = models.DateTimeField()
-    deposit_amount= models.FloatField(max_digits=10, decimal_places=2)
-    status= models.CharField(max_length=30, status=Status_posted)
+    deposit_amount= models.DecimalField(max_digits=10, decimal_places=2)
+    status= models.CharField(max_length=30, choices=Status_posted)
